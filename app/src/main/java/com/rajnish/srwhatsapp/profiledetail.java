41f992ed0c7e2ca0e6c0ab25e76eb00dc708641e
Rajnish <kumarrajnish.stm@gmial.com>
2022-06-21 14:26:08 +0530

    new

---
+++ app/src/main/java/com/rajnish/srwhatsapp/profiledetail.java
@@ -0,0 +1,14 @@
+package com.rajnish.srwhatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profiledetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profiledetail);
+    }
+}
